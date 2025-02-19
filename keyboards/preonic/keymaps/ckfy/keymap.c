a4095d8f2db57e929afc9fa83ce96f5fbd9d8b6d
Nelson Chan <chakflying@hotmail.com>
2025-02-19 20:42:10 +0800

    Upgrade config and functions to new version

--- keyboards/preonic/keymaps/ckfy/keymap.c
+++ keyboards/preonic/keymaps/ckfy/keymap.c
@@ -184,7 +184,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT_preonic_grid(
   KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  _______, QK_BOOT, DB_TOGG,   CK_ON,  CK_OFF,  GUI_ON, GUI_OFF, UC_NEXT, _______,  _______, _______,  KC_DEL,
+  _______, QK_BOOT, DB_TOGG,   CK_ON,  CK_OFF,   GU_ON,  GU_OFF, UC_NEXT, _______,  _______, _______,  KC_DEL,
   QK_BOOT, _______, MU_NEXT,   AU_ON,  AU_OFF, AG_NORM, AG_SWAP,  QWERTY, COLEMAK,   DVORAK, _______, _______,
   _______, AU_PREV, AU_NEXT,   MU_ON,  MU_OFF,   MI_ON,  MI_OFF, _______, _______,  _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, RGB_VAI, RGB_RMOD, RGB_MOD, RGB_TOG
@@ -204,7 +204,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_EXTRA] = LAYOUT_preonic_grid(
-  KC_TILD, X(LAUGHCRY),    X(XD), X(WARM), X(ROFL), X(TEARSAD), X(CRYING), X(EXPLODE), X(SPEAKNOEVIL),  X(BOW),    KC_0,  KC_BSPC,
+  KC_TILD, UM(LAUGHCRY),  UM(XD), UM(WARM), UM(ROFL), UM(TEARSAD), UM(CRYING), UM(EXPLODE), UM(SPEAKNOEVIL),  UM(BOW),    KC_0,  KC_BSPC,
   KC_TAB,      _______,   KC_INS, KC_HOME,   KC_UP,     KC_END,   KC_PGUP,      KC_P7,          KC_P8,   KC_P9, KC_PPLS,   KC_DEL,
   KC_CAPS,     _______,   KC_DEL, KC_LEFT, KC_DOWN,   KC_RIGHT,   KC_PGDN,      KC_P4,          KC_P5,   KC_P6, KC_PMNS,  KC_PIPE,
   _______,     KC_PSLS,  KC_PAST, KC_PMNS, KC_PPLS,    KC_PSCR,    KC_NUM,      KC_P1,          KC_P2,   KC_P3, KC_PDOT,  _______,
--- keyboards/preonic/keymaps/ckfy/readme.md
+++ keyboards/preonic/keymaps/ckfy/readme.md
@@ -1,1 +1,17 @@
 # The default Preonic layout - largely based on the Planck's
+
+# Build Steps
+
+- Open MSYS MINGW64
+
+### Compile
+
+```shell
+qmk compile -kb preonic/rev3 -km ckfy
+```
+
+### Flash
+
+```shell
+qmk flash -kb preonic/rev3 -km ckfy
+```
